958cffe30113a9ec81777145f4a4edf0fe65672a
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-19 08:42:27 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("sizeof int: %d" sizeof(int));
+	return (0);
+}
