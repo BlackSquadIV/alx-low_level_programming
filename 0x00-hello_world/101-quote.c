1f001b63e4d48e4f28030cf2b2ae2cea38caa787
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-19 10:11:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that prints a line to standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
