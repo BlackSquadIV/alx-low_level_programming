5b3a75df79a6c5f8d59d48695aa519d8325e8290
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-21 22:31:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints lowercase alphabets in reverse
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	int i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+
+	for (i = 26; i >= 1; i--)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+
+return (0);
+}
