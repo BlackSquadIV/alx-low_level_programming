8f815b851e4431bf13017207413ce69dd76e2fad
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-21 22:15:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - To print numbers without using char function
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	putchar(n + 48);
+	}
+	putchar('\n');
+return (0);
+}
