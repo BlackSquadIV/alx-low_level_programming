c6da5142506a4f01ba8f36dca4dca46ace19e1e0
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-22 06:36:42 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - To print numbers in base 16 in lowercase
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	char num[] = "0123456789";
+	char alp[] = "abcdef";
+	int i = 16;
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar(num[i]);
+		putchar(alp[i]);
+	}
+	putchar('\n')
+return (0);
+}
