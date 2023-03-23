147adbe2ae41ae701a3d66bbe96b35a5c02b0e54
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-23 08:15:47 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase, ten times.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int n;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (n = 1; n <= 10; n++)
+		{
+		continue;
+		}
+	_putchar(i);
+	}
+	_putchar('\n');
+}
