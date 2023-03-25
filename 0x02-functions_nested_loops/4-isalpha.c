113f10770141475bb011eca4b576b230c1cebd90
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-24 20:14:40 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabetic character
  *
- * int c: parameter to be printed
+ * @c: parameter to be printed
  *
  * Return: 1 if c is a letter, lowercase or uppercase
  * and 0 if otherwise
@@ -10,13 +10,8 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		if (c >= 65 && c <= 90)
-		{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
-		}
 	else
-	return (0);
-	}
+		return (0);
 }
