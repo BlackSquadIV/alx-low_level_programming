46bc2ce0dacf74cf3f5b8ffdf05a4a6519423552
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-25 11:34:00 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - to print the sign of numbers
+ *
+ * @n: function parameter
+ *
+ * Return: 0, 1 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	_putchar(',');
+	_putchar(' ');
+	return (1);
+	else if (n == 0)
+		{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+		}
+		else
+		{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+		}
+	}
+}
