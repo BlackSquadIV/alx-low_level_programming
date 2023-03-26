da831ed2c943ac89aac5bdae0ca239d3f6acd845
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-25 22:54:40 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @n: parameter to be printed
+ *
+ * Return: Always 0
+ *
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+	_putchar(n);
+	return (0);
+	}
+	else if (n < 0)
+	{
+	_putchar(n);
+	return (0);
+	}
+	else
+	{
+	_putchar(n);
+	return (0);
+	}
+}
