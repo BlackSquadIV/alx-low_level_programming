fd9a72e311530b864e58c339899c7a56be168368
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-25 23:07:15 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,19 +11,9 @@
 
 int _abs(int n)
 {
-	if (n > 0)
-	{
-	_putchar(n);
-	return (0);
-	}
+	if (n >= 0)
+	n = n;
 	else if (n < 0)
-	{
-	_putchar(n);
-	return (0);
-	}
-	else
-	{
-	_putchar(n);
-	return (0);
-	}
+	n = -(n);
+	return (n);
 }
