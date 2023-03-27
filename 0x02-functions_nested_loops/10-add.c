5f7045853f999482e3878b84cac41a1f8ff18167
BlackSquad4 <ejimfughaoluebube@gmail.com>
2023-03-27 07:51:56 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - A program that adds two numbers
+ *
+ *@a: function parameter
+ *@b: function parameter
+ *
+ * Return: k
+ */
+
+int add(int a, int b)
+{
+	int k = (a + b);
+
+	return (k);
+}
